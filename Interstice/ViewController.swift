8b1e658459914d880a720d8a695723224cf5b813
Stephanie Cappello <stephanie.cappello@thoughtworks.com>
2019-08-20 14:01:26 -0500

    first commit

---
+++ Interstice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interstice
+//
+//  Created by Stephanie Cappello on 8/20/19.
+//  Copyright © 2019 StarSteppers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
