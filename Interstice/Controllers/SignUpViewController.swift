6d6a5cd9548e06e2ad7c7a7d380c0a6fd0ceb864
Stephanie Cappello <stephanie.cappello@thoughtworks.com>
2019-09-04 14:37:37 -0500

    [#2] Splash Screen Finished

--- Interstice/Controllers/SignUpViewController.swift
+++ Interstice/Controllers/SignUpViewController.swift
@@ -1,20 +1,19 @@
 //
-//  ViewController.swift
+//  SignUpViewController.swift
 //  Interstice
 //
-//  Created by Stephanie Cappello on 8/20/19.
+//  Created by Stephanie Cappello on 9/3/19.
 //  Copyright © 2019 StarSteppers. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
